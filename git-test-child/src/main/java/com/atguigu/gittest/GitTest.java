8536b11231416de7992148962567b47ec4239792
sooneven <2721424080@qq.com>
2022-05-15 22:37:50 +0800

    master commit test

--- git-test-child/src/main/java/com/atguigu/gittest/GitTest.java
+++ git-test-child/src/main/java/com/atguigu/gittest/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
         System.out.println("hot-fix-hello-git4");
+        System.out.println("mast-test");
     }
 }
