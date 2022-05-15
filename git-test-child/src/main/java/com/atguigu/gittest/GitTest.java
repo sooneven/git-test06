b2e7ee5eee9b65305b4a04c446cfd584f043a115
sooneven <2721424080@qq.com>
2022-05-15 22:38:29 +0800

    hot-fix commit test

--- git-test-child/src/main/java/com/atguigu/gittest/GitTest.java
+++ git-test-child/src/main/java/com/atguigu/gittest/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
+        System.out.println("hot-fix-hello-git4");
         System.out.println("hot-fix-test");
     }
 }
